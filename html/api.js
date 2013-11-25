f480a5a2fb2f388617f16861e2f05f3dc84b7f22
Erik <ralree@gmail.com>
2013-11-24 19:14:01 -0500

    Fixing up JS some more.

--- html/api.js
+++ html/api.js
@@ -1,6 +1,5 @@
 function forAllRPCs(func) {
-    $('#info tr[class="rpc"]').remove();
-    $.getJSON( "/api", function( data ) {
+    $.getJSON( "api", function( data ) {
         for(i=0; i<data.length; ++i) {
             func(data[i]);
         }
